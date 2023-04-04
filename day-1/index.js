e1ee8d34a7deb4fc3d7434e670fc55f22048353a
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-04 16:27:39 +0500

    day-1

---
+++ day-1/index.js
@@ -0,0 +1,40 @@
+// var cubeChecker = function (volume, side) {
+//   if (side ** 3 == volume && side > 0 && volume > 0) {
+//     return true;
+//   } else return false;
+// };
+// console.log(cubeChecker(-27, -3));
+
+// =========================================================
+// masala sharti. link: https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/javascript
+
+// function solution(nums) {
+//   if (nums == null) {
+//     return [];
+//   } else {
+//     return nums.sort((a, b) => a - b);
+//   }
+// }
+// console.log(solution(null));
+
+// ==========================================================
+// 6 kyu
+// masala sharti. link:https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
+
+// function likes(names) {
+//   if (names.length == 0) {
+//     return `no one likes this`;
+//   } else if (names.length == 1) {
+//     return `${names[0]} likes this`;
+//   } else if (names.length == 2) {
+//     return `${names[0]} and ${names[1]} like this`;
+//   } else if (names.length == 3) {
+//     return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//   } else {
+//     return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+//   }
+// }
+// console.log(likes([]));
+
+// masala sharti. link:https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
+// 6 kyu
