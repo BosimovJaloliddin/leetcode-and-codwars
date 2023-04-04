50b901355303d7f057817606039db03deff83c31
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-05 02:01:42 +0500

    day-1

--- day-1/index.js
+++ day-1/index.js
@@ -36,5 +36,108 @@
 // }
 // console.log(likes([]));
 
-// masala sharti. link:https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
+// =======================================================
+// masala sharti. link:https://www.codewars.com/kata/596fba44963025c878000039/train/javascript
+// 8 kyu
+
+// function contamination(text, char) {
+//   if (text == "" || char == "") {
+//     return ``;
+//   } else {
+//     let res = "";
+//     for (let i = 1; text.length >= i; i++) {
+//       res += char;
+//     }
+//     return res;
+//   }
+// }
+
+// console.log(contamination("wd", ""));
+
+// ===========================================================
+// masala sharti. link:https://www.codewars.com/kata/51e0007c1f9378fa810002a9/train/javascript
 // 6 kyu
+
+// function parse(data) {
+//   let ar = [];
+//   let sum = 0;
+//   Array.from(data, (v) => {
+//     console.log(v);
+//     if (v === "i") {
+//       sum++;
+//     } else if (v === "d") {
+//       sum--;
+//     } else if (v === "s") {
+//       sum = sum ** 2;
+//     } else if (v === "o") {
+//       ar.push(sum);
+//     }
+//   });
+//   return ar;
+// }
+
+// parse("iiisdoso");
+
+// ======================================================
+// masala sharti. link:https://www.codewars.com/kata/56d19b2ac05aed1a20000430/train/javascript
+
+// function betweenExtremes(numbers) {
+//   let sortNum = numbers.sort((a, b) => a - b);
+//   return sortNum[sortNum.length - 1] - sortNum[0];
+// }
+// console.log(betweenExtremes([2, 34, 56, 42, 2, 1]));
+
+// =======================================================
+// masala sharti. link:https://www.codewars.com/kata/586beb5ba44cfc44ed0006c3/train/javascript
+
+// function sumEvenNumbers(input) {
+//   let sum = 0;
+//   input.forEach((v) => {
+//     if (v % 2 == 0) {
+//       sum += v;
+//     }
+//   });
+//   return sum;
+// }
+
+// console.log(sumEvenNumbers([4, 4, 5, 6]));
+
+// ========================================================
+// masala sharti. link:https://www.codewars.com/kata/5626b561280a42ecc50000d1/train/javascript
+// 6 kyu
+
+// function sumDigPow(a, b) {
+//   let ar = [];
+//   for (let i = a; b >= i; i++) {
+//     let str = i.toString();
+//     if (str.length < 2) {
+//       ar.push(i);
+//     } else {
+//       let sum = 0;
+//       for (let j = 0; str.length > j; j++) {
+//         sum += str[j] ** (j + 1);
+//       }
+//       if (sum == +str) {
+//         ar.push(sum);
+//       }
+//       sum = 0;
+//     }
+//   }
+//   return ar;
+// }
+// console.log(sumDigPow(363, 2427));
+
+// ================================================================
+// masala sharti. link:https://www.codewars.com/kata/5949481f86420f59480000e7/train/javascript
+// 7 kyu
+
+// function oddOrEven(array) {
+//   let sum = 0;
+//   array.forEach((v) => (sum += v));
+//   if (Math.abs(sum) % 2 == 0) return `even`;
+//   else return `odd`;
+// }
+
+// console.log(oddOrEven([0, -9, -8]));
+
+// ===============================================================
