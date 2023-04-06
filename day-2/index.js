3e531b4956f0058b9e401843f7a2d15dfbabacd4
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-06 18:06:58 +0500

    day-2

---
+++ day-2/index.js
@@ -0,0 +1,11 @@
+// masala shartiga link:https://www.codewars.com/kata/62c93765cef6f10030dfa92b/train/javascript
+// 7 kyu
+
+// function solution(start, finish) {
+//   let step = finish - start;
+//   let getStepThere = Math.trunc(step / 3);
+//   let getStepOne = step % 3;
+//   return getStepThere + getStepOne;
+// }
+
+// console.log(solution(2, 7));
