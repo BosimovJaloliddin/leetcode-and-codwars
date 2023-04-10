8eac793fdcd7e910da561792e610b33fe60d0cc5
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-10 21:21:05 +0500

    day-3

---
+++ day-3/index.js
@@ -0,0 +1,51 @@
+// masala shartiga link:https://www.codewars.com/kata/5b077ebdaf15be5c7f000077/train/javascript
+// 8 kyu
+
+// var countSheep = function (num) {
+//   let res = "";
+//   for (let i = 1; i <= num; i++) {
+//     res += `${i} sheep...`;
+//   }
+//   return res;
+//   //your code here
+// };
+// countSheep(0);
+
+// ==================================
+// masala shartiga link:https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/train/javascript
+//7 kyu
+
+// function sortByLength(array) {
+//   let res = array.sort((a, b) => a.length - b.length);
+//   return res;
+// }
+
+// sortByLength(["Beg", "Life", "I", "To"]);
+
+// ===================================
+// masala shartiga link:https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/javascript
+// 7 kyu
+
+// function noOdds(values) {
+//   return values.filter((v) => v % 2 === 0);
+// }
+
+// console.log(noOdds([0, 1, 2, 3, 4]));
+
+// ====================================
+// masala shartiga link:https://www.codewars.com/kata/53d32bea2f2a21f666000256/train/javascript
+// 7 kyu
+
+// function largest(n, array) {
+//   let res = [];
+//   array
+//     .sort((a, b) => b - a)
+//     .forEach((v, i) => {
+//       if (i < n) {
+//         res.push(v);
+//       }
+//     });
+//   return res.reverse();
+// }
+
+// console.log(largest(5, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
