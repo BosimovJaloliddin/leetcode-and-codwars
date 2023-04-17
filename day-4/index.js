83ec8e49951ad03a29d4bc0065affa74d7bb840e
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-17 17:37:02 +0500

    day-4

---
+++ day-4/index.js
@@ -0,0 +1,12 @@
+// masala shartiga link:https://www.codewars.com/kata/534d0a229345375d520006a0/train/javascript
+// 7 kyu
+
+// function isPowerOfTwo(n) {
+//   if (n / 2 === 0 && n % 2 === 0) {
+//     return true;
+//   } else {
+//     return isPowerOfTwo(n / 2);
+//   }
+//   //.. should return true or false ..
+// }
+// console.log(isPowerOfTwo(2));
