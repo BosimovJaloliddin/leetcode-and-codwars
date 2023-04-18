589cd80d41808127e39e48e78471da30ea02ca28
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-18 14:09:03 +0500

    day-6

---
+++ day-6/index.js
@@ -0,0 +1,12 @@
+//masala shartiga link:https://www.codewars.com/kata/55caf1fd8063ddfa8e000018/train/javascript
+// 7 kyu
+// function arithmeticSequenceElements(a, d, n) {
+//   let res = [];
+//   res.push(a);
+//   for (let i = 1; i < n; i++) {
+//     res.push(res[res.length - 1] + d);
+//   }
+//   return res.join(", ");
+// }
+
+// console.log(arithmeticSequenceElements(1, 2, 4));
