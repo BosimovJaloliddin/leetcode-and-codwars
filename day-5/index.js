01e897286ee43f6f6b964627f39526aadfad982c
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-18 13:34:45 +0500

    day-5

--- day-5/index.js
+++ day-5/index.js
@@ -1,2 +1,16 @@
-// masala sharti linkda:https://www.codewars.com/kata/570597e258b58f6edc00230d/train/javascript
+// masala sharti linkda:https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
 // 8 kyu
+// function removeChar(str) {
+//   let res = str.slice(1, -1);
+//   return res;
+// }
+// console.log(removeChar("eloquent"));
+
+// =============================================
+// masala sharti linkda:https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/javascript
+// 6 kyu
+
+// const findUniq = (arr) =>
+//   arr.sort((a, b) => a - b).filter((v) => v !== arr[1])[0];
+
+// console.log(findUniq([10, 3, 3, 3]));
