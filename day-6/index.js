b7b1cc6b6eeaecae0214e7463aa9d88a16fdd961
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-18 20:20:01 +0500

    day-6

--- day-6/index.js
+++ day-6/index.js
@@ -10,3 +10,27 @@
 // }
 
 // console.log(arithmeticSequenceElements(1, 2, 4));
+
+// ==================================================
+// masala shartiga link:https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+// 5 kyu
+
+// function pigIt(str) {
+//   res = "";
+//   let arr = str.split(" ");
+//   arr.forEach((v, i) => {
+//     if (v.toUpperCase() == v.toLowerCase()) {
+//       res += v;
+//     } else if (arr.length - 1 === i) {
+//       res += v.slice(1, v.length);
+//       res += v.slice(0, 1) + "ay";
+//     } else {
+//       res += v.slice(1, v.length);
+//       res += v.slice(0, 1) + "ay";
+//       res += " ";
+//     }
+//   });
+//   return res;
+// }
+
+// console.log(pigIt("P latin is cool ?"));
