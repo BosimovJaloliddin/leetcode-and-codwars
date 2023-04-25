938e03413be4c202849c1e7fc0c5077e18988146
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-25 14:00:58 +0500

    No String

--- day-7/1-masala/index.js
+++ day-7/1-masala/index.js
@@ -1,3 +1,6 @@
+// masala shartig link:https://www.codewars.com/kata/584c7b1e2cb5e1a727000047/train/javascript
+// 7 kyu
+
 const helloWorld = () => {
   let a = String(/Hello, World!/);
   a = a.substring(1, a.length - 1);
