77109ed867511991616c4f78395a3debe8eb65e1
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-26 22:58:39 +0500

    arraysSimilar

--- day-7/5-masala/index.js
+++ day-7/5-masala/index.js
@@ -1,6 +1,7 @@
 // masala shartiga link:https://www.codewars.com/kata/51e704f2d8dbace389000279/train/javascript
 // 6 kyu
 
+// ===============================
 function arraysSimilar(arr1, arr2) {
   let sort1 = arr1.sort((a, b) => a - b);
   let sort2 = arr2.sort((a, b) => a - b);
@@ -13,3 +14,10 @@ function arraysSimilar(arr1, arr2) {
   return sum == sort1.length && sum == sort2.length ? true : false;
 }
 console.log(arraysSimilar([1, 2, 2, 3, 4, 3], [2, 1, 2, 4, 3]));
+
+// ================================
+function arraysSimilar(arr1, arr2) {
+  arr1.sort();
+  arr2.sort();
+  return arr1.length === arr2.length && arr1.every((a, i) => a === arr2[i]);
+}
---
+++ day-8/1-masala/index.js
@@ -0,0 +1,22 @@
+// masala shartiga link:https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/javascript
+// 6 kyu
+
+multiplicationTable = function (size) {
+  let res = [];
+  let count = 1;
+  if (size === 1) res.push([1]);
+  else {
+    for (let i = 1; size >= i; i++) {
+      let analiz = i;
+      let arr = [];
+      res.push(arr);
+      for (let j = 0; j < size; j++) {
+        arr.push(count);
+        count += i;
+      }
+      count = analiz + 1;
+    }
+  }
+  return res;
+};
+console.log(multiplicationTable(5));
