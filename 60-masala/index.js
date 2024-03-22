01bcfd3a2ed0fdc368641de5dfe44556e8ccd02c
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-03-22 20:44:12 +0500

    2703. Return Length of Arguments Passed

--- 60-masala/index.js
+++ 60-masala/index.js
@@ -1,1 +1,30 @@
-console.log("Hellow World");
+// leetcode => 2703. Return Length of Arguments Passed
+// link => https://leetcode.com/problems/return-length-of-arguments-passed/description/
+
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+// Example 1:
+
+// Input: args = [5]
+// Output: 1
+// Explanation:
+// argumentsLength(5); // 1
+
+// One value was passed to the function so it should return 1.
+// Example 2:
+
+// Input: args = [{}, null, "3"]
+// Output: 3
+// Explanation:
+// argumentsLength({}, null, "3"); // 3
+
+// Three values were passed to the function so it should return 3.
+
+// ============================================= Easy ===========================
+
+let args = [{}, null, "3"];
+var argumentsLength = function (...args) {
+  return args.flat(Infinity).length;
+};
+
+console.log(argumentsLength(args));
