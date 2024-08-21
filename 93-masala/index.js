9cba66dcde6af4e1f22eea1294cc718b3aad8e76
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-08-21 17:25:29 +0500

    2677. Chunk Array

---
+++ 93-masala/index.js
@@ -0,0 +1,26 @@
+// var chunk = function (arr, size) {
+//   let res = [];
+//   let a = [];
+//   arr.forEach((v, i) => {
+//     if ((i + 1) % size == 0 || arr.length - 1 === i) {
+//       a.push(v);
+//       res.push(a);
+//       a = [];
+//     } else {
+//       a.push(v);
+//     }
+//   });
+//   return res;
+// };
+
+var chunk = function (arr, size) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+
+  return result;
+};
+
+console.log(chunk([8, 5, 3, 2, 6], 2));
