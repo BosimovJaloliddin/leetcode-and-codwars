22dd1f766394931fb58cc97ff4e66191451eb16a
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-10-19 13:53:42 +0500

    tugamadi

---
+++ 95-masala/index.js
@@ -0,0 +1,21 @@
+// 3099. Harshad Number
+// link => https://leetcode.com/problems/harshad-number/description/
+var sumOfTheDigitsOfHarshadNumber = function (x) {
+  let res = 0;
+  if (x < 10) {
+    res = x;
+  } else if (x === 10) {
+    res = 1;
+  } else
+    for (let i = 2; i <= x; i++) {
+      if (x % i === 0) {
+        res = x / i;
+        break;
+      } else if (x === i) {
+        res = -1;
+      }
+    }
+  return res;
+};
+
+console.log(sumOfTheDigitsOfHarshadNumber(12));
